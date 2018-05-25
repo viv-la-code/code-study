ba7c263b4ec5fb8eca5aff72a852005d6dc97ff1
bradlm <bradlm@viv.ai>
2018-05-25 13:24:58 -0700

    problems for chapter 2 and 3 added

---
+++ problems/eloquentJS/chapter 2 - program structure/2.1.js
@@ -0,0 +1,10 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+//your code here
---
+++ problems/eloquentJS/chapter 3 - functions/3.1.js
@@ -0,0 +1,3 @@
+//The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+
+//your code here
