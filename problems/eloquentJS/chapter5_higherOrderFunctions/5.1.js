9dfa49c99de58164197da64da26490f9c49ddfc3
bradlm <bradlm@viv.ai>
2018-05-29 13:17:42 -0700

    ch5 solutions

--- problems/eloquentJS/chapter5_higherOrderFunctions/5.1.js
+++ problems/eloquentJS/chapter5_higherOrderFunctions/5.1.js
@@ -1,2 +1,14 @@
 // Flattening
 // Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
+
+//your code here
+
+//rewrote to extend the array object
+
+Array.prototype.flatten = function () {
+  return this.reduce((a, b) => a.concat(Array.isArray(b) ? b.flatten() : b), [])
+}
+
+console.log([[1, 2, 3], [4, 5], [6]].flatten())
+
+// → [1, 2, 3, 4, 5, 6]
--- problems/eloquentJS/chapter5_higherOrderFunctions/5.2.js
+++ problems/eloquentJS/chapter5_higherOrderFunctions/5.2.js
@@ -2,3 +2,15 @@
 // Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. And finally, it calls the update function to create a new value and starts from the beginning.
 
 // When defining the function, you can use a regular loop to do the actual looping.
+
+// Your code here.
+
+function loop(start, end, step, cb) {
+  for(let i = start; end(i); i = step(i)) {
+    console.log(i)
+  }
+}
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
